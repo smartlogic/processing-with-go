747b8812c4358371c85ea6a0d4e7beea17315d36
Eric Oestrich <eric@oestrich.org>
2014-02-10 17:41:13 -0500

    Apply a theme and rearrange slides

---
+++ smartlogic.rb
@@ -0,0 +1,10 @@
+@slide_footer_info_right_text = "SmartLogic"
+
+@x_large_font_size = screen_size(5 * Pango::SCALE)
+@large_font_size = screen_size(4.0 * Pango::SCALE)
+@normal_font_size = screen_size(3.25 * Pango::SCALE)
+@small_font_size = screen_size(3.0 * Pango::SCALE)
+@xx_small_font_size = screen_size(2.5 * Pango::SCALE)
+
+include_theme("slide-footer-info")
+include_theme("default")
